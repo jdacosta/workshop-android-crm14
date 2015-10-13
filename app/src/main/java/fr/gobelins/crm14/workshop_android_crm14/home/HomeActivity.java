2e6e81e589c830d13478378ee064ecaecf61abe3
Julien Martins Da Costa <contact@julien-dacosta.fr>
2015-10-13 14:34:20 +0200

    Add LoginFragment on HomeActivity

--- app/src/main/java/fr/gobelins/crm14/workshop_android_crm14/home/HomeActivity.java
+++ app/src/main/java/fr/gobelins/crm14/workshop_android_crm14/home/HomeActivity.java
@@ -2,14 +2,65 @@ package fr.gobelins.crm14.workshop_android_crm14.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import fr.gobelins.crm14.workshop_android_crm14.R;
+import fr.gobelins.crm14.workshop_android_crm14.home.fragments.LoginFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // instantiate toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.homeToolbar);
+        setSupportActionBar(toolbar);
+
+        // instantiate login fragment
+        loginFragment();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuHomeLoginItem) {
+            loginFragment();
+            return true;
+        }
+        else if (item.getItemId() == R.id.menuHomeRegisterItem) {
+            //registerFragment();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    private void loginFragment() {
+        getSupportFragmentManager()
+            .beginTransaction()
+            .add(R.id.homeContainer, new LoginFragment())
+            .commit();
+
+        getSupportActionBar().setTitle(R.string.R_string_home_toolbar_login_title);
+    }
+
+    /*private void registerFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.homeContainer, new RegisterFragment())
+                .commit();
+
+        getSupportActionBar().setTitle(R.string.R_string_home_toolbar_register_title);
+    }*/
 }
---
+++ app/src/main/java/fr/gobelins/crm14/workshop_android_crm14/home/fragments/LoginFragment.java
@@ -0,0 +1,78 @@
+package fr.gobelins.crm14.workshop_android_crm14.home.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import fr.gobelins.crm14.workshop_android_crm14.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class LoginFragment extends Fragment {
+
+    private static final String TAG = "LoginFragment";
+    private OnFragmentInteractionListener mListener;
+
+    @Bind(R.id.homeLoginEmailField) TextView homeLoginEmailField;
+    @Bind(R.id.homeLoginPasswordField) TextView homeLoginPasswordField;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (OnFragmentInteractionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+        ButterKnife.unbind(this);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+    }
+
+}

3f3d0924e8c351de9320090161f64e47e57dc3e0
risq <valentin.ledrapier@gmail.com>
2015-10-13 14:45:36 +0200

    Add splashActivity intent

--- app/src/main/java/fr/gobelins/crm14/workshop_android_crm14/splash/SplashActivity.java
+++ app/src/main/java/fr/gobelins/crm14/workshop_android_crm14/splash/SplashActivity.java
@@ -1,9 +1,11 @@
 package fr.gobelins.crm14.workshop_android_crm14.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import fr.gobelins.crm14.workshop_android_crm14.R;
+import fr.gobelins.crm14.workshop_android_crm14.home.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        loadHomeActivity();
+    }
+
+    private void loadHomeActivity() {
+        Intent homeActivityIntent = new Intent(this, HomeActivity.class);
+        startActivity(homeActivityIntent);
     }
 }
